1f59271e2d2b3d64559ea81ba4b3864a95fc7c46
Yanina <yaninagarcia.ana@gmail.com>
2022-06-13 22:54:19 -0300

    componente About

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { NavBar } from "./Components/NavBar";
 import Home  from "./Components/Home";
+import About from "./Components/About";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <NavBar/>
       <Home/>
+      <About/>
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-slate-900'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
         {/*contenedor*/} {/* mx- margin px_padding */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-[#c6488c]'>Hola, mi nombre es</p>
