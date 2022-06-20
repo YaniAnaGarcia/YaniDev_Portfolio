6c4b13811b5ee18c30f02ebfdea85390c57f378b
Yanina <yaninagarcia.ana@gmail.com>
2022-06-19 22:52:58 -0300

    ultimando detalles

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
             <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div className='sm:text-right text-4xl font-bold'>
                         <p>
-                            Developer en proceso. Con marcada orientacion al Desarrollo Front End.
+                            Full stack developer, con marcada orientacion al Desarrollo Front End.
                         </p>
                     </div>
                     <div>
