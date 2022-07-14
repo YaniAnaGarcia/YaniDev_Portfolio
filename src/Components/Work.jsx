6b09d8f8fbc0e86b159024d098fa38a0fe813cca
Yanina <yaninagarcia.ana@gmail.com>
2022-07-13 21:59:18 -0300

    arreglos de codigo

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -9,7 +9,7 @@ import {HiLink} from 'react-icons/hi';
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] py-2 my-auto'>
         {/* container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             {/* encabezado */}
